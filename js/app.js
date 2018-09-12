d5381937c533ff30f0ef03935d31442277af7746
jaimin <jaimin.ece@gmail.com>
2018-09-12 01:25:51 -0400

    Files Added

---
+++ js/app.js
@@ -0,0 +1,25 @@
+
+
+$('#portfolio-tab').on('click', function () {
+  $('#portfolio-content').show(500);
+    $('#home').hide(500);
+    $('#Skills').hide(500);
+    
+  });
+
+  $('#home-tab').on('click', function () {
+    $('#home').show(500);
+    $('#portfolio-content').hide(500);
+    $('#Skills').hide(500);
+  });
+
+
+  $('#skills-tab').on('click', function () {
+    $('#Skills').show(500);
+    $('#home').hide(500);
+    $('#portfolio-content').hide(500);
+  });
+
+
+
+ 
